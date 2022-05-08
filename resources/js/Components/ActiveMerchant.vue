78a01e9d28678bf33fd6465ea87fd01e18de5dd9
Tim Chen <Tim.chen@outlook.de>
2022-05-08 15:59:50 +0200

    finished merchant page and added zone images

---
+++ resources/js/Components/ActiveMerchant.vue
@@ -0,0 +1,82 @@
+<script setup>
+
+import {computed, ref} from "vue";
+
+const props = defineProps([
+    'name',
+    'zone',
+    'card',
+    'rapport',
+    'votes'
+])
+
+const cardRarityClassObject = computed(() => ({
+    'tw-text-lime-500': props.card.rarity === 1,
+    'tw-text-blue-500': props.card.rarity === 2,
+    'tw-text-purple-500': props.card.rarity === 3,
+    'tw-text-amber-500': props.card.rarity === 4,
+}))
+
+
+let nextReset = new Date();
+nextReset.setMinutes(55);
+nextReset.setSeconds(0);
+
+const imgUrl = '/assets/zones/'+props.zone+'.jpg'
+const dialog = ref(false);
+
+</script>
+
+<template>
+    <v-list-item class="tw-flex tw-bg-neutral-100 dark:tw-bg-neutral-600 tw-mt-3">
+        <v-col cols="4">
+            <v-list-item-title class="dark:tw-text-white">{{ name }}</v-list-item-title>
+            <!--
+
+            -->
+
+            <v-dialog
+                v-model="dialog"
+                width="500"
+            >
+                <template v-slot:activator="{ on, attrs }">
+                    <a class="tw-cursor-pointer" @click="dialog = true">
+                        <v-list-item-subtitle class="zone-name dark:tw-text-neutral-100">
+                            {{ zone }}
+                        </v-list-item-subtitle></a>
+                </template>
+
+                <v-card class="tw-bg-white dark:tw-bg-neutral-800 tw-p-4">
+                    <v-card-title class="dark:tw-text-white">
+                        {{zone}}
+                    </v-card-title>
+
+                    <v-card-text>
+                        <img :src="imgUrl" :alt="zone">
+                    </v-card-text>
+                </v-card>
+            </v-dialog>
+
+        </v-col>
+
+        <div class="tw-flex tw-w-1/4 justify-center align-center">
+            <p class="mr-1" :class="cardRarityClassObject">{{ card.name }}</p>
+        </div>
+        <div class="tw-flex justify-end align-center tw-ml-auto">
+            <div v-if="rapport.rarity === 3"
+                 style="background-image: url('https://lostarkcodex.com/images/icon_grade_3.webp');">
+                <img src="https://lostarkcodex.com/icons/shop_icon_17.webp" alt="Rapport Chest" style="height: 40px">
+            </div>
+            <div v-if="rapport.rarity === 4"
+                 style="background-image: url('https://lostarkcodex.com/images/icon_grade_4.webp');">
+                <img src="https://lostarkcodex.com/icons/shop_icon_17.webp" alt="Rapport Chest" style="height: 40px">
+            </div>
+        </div>
+
+        <div class="tw-flex justify-center align-center tw-ml-auto dark:tw-text-white">
+            <p>{{votes}} Votes</p>
+        </div>
+
+
+    </v-list-item>
+</template>
---
+++ resources/js/Helpers/merchants.js
@@ -0,0 +1,125 @@
+import * as signalR from "@microsoft/signalr";
+
+const connection = new signalR.HubConnectionBuilder()
+    .configureLogging(signalR.LogLevel.Debug)
+    .withUrl("https://lostmerchants.com/MerchantHub", {
+        skipNegotiation: true,
+        transport: signalR.HttpTransportType.WebSockets
+    })
+    .build();
+
+export async function start() {
+    try {
+        await connection.start();
+        console.log("SignalR Connected.");
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+
+connection.onclose(async () => {
+    await start();
+});
+
+export async function getMerchants(server) {
+    try {
+        let merchants = await connection.invoke("GetKnownActiveMerchantGroups", server);
+        return merchants;
+    } catch (err) {
+        console.log(err);
+        setTimeout(start, 5000);
+    }
+};
+
+
+export const regions =
+    [
+        {
+            name: 'EU Central',
+            servers:
+                [
+                'Neria',
+                'Kadan',
+                'Trixion',
+                'Calvasus',
+                'Thirain',
+                'Zinnervale',
+                'Asta',
+                'Wei',
+                'Slen',
+                'Procyon',
+                'Sceptrum',
+                'Beatrice',
+                'Inanna',
+                'Thaemine',
+                'Sirius',
+                'Antares',
+                'Brelshaza',
+                'Nineveh',
+                'Mokoko',
+                ]
+        },
+        {
+            name: 'EU West',
+            servers: [
+                'Rethramis',
+                'Tortoyk',
+                'Moonkeep',
+                'Stonehearth',
+                'Shadespire',
+                'Tragon',
+                'Petrania',
+                'Punika',
+            ]
+        },
+        {
+            name: 'NA East',
+            servers: [
+                'Azena',
+                'Una',
+                'Regulus',
+                'Avesta',
+                'Galatur',
+                'Karta',
+                'Ladon',
+                'Kharmine',
+                'Elzowin',
+                'Sasha',
+                'Adrinne',
+                'Aldebaran',
+                'Zosma',
+                'Vykas',
+                'Danube',
+            ]
+        },
+        {
+            name: 'NA West',
+            servers: [
+                'Mari',
+                'Valtan',
+                'Enviska',
+                'Akkan',
+                'Bergstrom',
+                'Shandi',
+                'Rohendel',
+            ]
+        },
+        {
+            name: 'South America',
+            servers: [
+                'Kazeros',
+                'Agaton',
+                'Gienah',
+                'Arcturus',
+                'Yorn',
+                'Feiton',
+                'Vern',
+                'Kurzan',
+                'Prideholme',
+            ]
+        }
+
+
+    ]
